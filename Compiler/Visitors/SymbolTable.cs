38c269d1265e7eca8d2a652f68b7511acf527fe5
Nima Moayedzadeh <57714574+Nima-Moayedzadeh@users.noreply.github.com>
2023-04-14 16:33:52 +0200

    Added Scope methods and start of SymbolTable Visitor

---
+++ Compiler/Visitors/SymbolTable.cs
@@ -0,0 +1,27 @@
+﻿using Moduino.analysis;
+using Moduino.node;
+
+namespace Compiler.Visitors;
+
+public class SymbolTable : DepthFirstAdapter
+{
+    private Scope currentScope;
+    private List<Scope> _scopes = new List<Scope>();
+    public SymbolTable()
+    {
+        currentScope = new Scope(null);
+        _scopes.Add(currentScope); //Global scope
+    }
+
+    public override void InANewFunc(ANewFunc node)
+    {
+        currentScope.addSymbol(node,Symbol.func);
+        currentScope = new Scope(currentScope);
+        _scopes.Add(currentScope);
+    }
+
+    public override void OutANewFunc(ANewFunc node)
+    {
+        currentScope = currentScope.getParent();
+    }
+}
