adafdaacde11f1910ff662506c90d30eba850342
AleksanderRDL <55142567+AleksanderRDL@users.noreply.github.com>
2023-04-24 10:18:47 +0200

    Basic typechecking should work now. Updated remaning tasks/status on them

--- Compiler/Visitors/TypeChecker.cs
+++ Compiler/Visitors/TypeChecker.cs
@@ -8,36 +8,43 @@ public class TypeChecker : DepthFirstAdapter
     // TASKS
     // 1.
     // Make sure to only store local variables and not the global ones from before
-    // Can be done by checking if currentScope's parent is not null (scope.parent != null) DONE?
+    // Can be done by checking if currentScope's parent is not null (scope.parent != null) 
+    // DONE
     
     // 2.
-    // Implement methods for getting a type from the nodes. (Type refers to Symbol enumerator)
-    // Right now node.GetType() will give the node's type (eg. ANeWFunc node) and not the type of the value in the node. DONE
+    // Fix getting a type from the nodes. (Type refers to Symbol enumerator)
+    // Right now node.GetType() will give the node's type (eg. ANeWFunc node) and not the type of the value in the node.
+    // DONE
 
     // 3 
     // Implement all the declarations, assignments and all nodes which need to be scanned by the visitor
     // I think control structures such as eg. for loops also will have local scopes so must be handled aswell (When/if we want to implement it)
+    // DONE
     
-    // 4 
+    // 4
+    // make sure you cant declare the same identifier twice
+    // DONE
+    
+    // 5 
     // Mangler at håndtere funktions parametre - De skal implementeres i CFG grammaren først (tror jeg?)
-    
-    // 5
-    // Check for forward referencing when checking assignment. If not declared before (also in global) it is an illegal assignment DONE
+    // To do
     
     // 6
-    // make sure you cant declare the same identifier twice
-
+    // Check for forward referencing when checking assignment. If not declared before (also in global) it is an illegal assignment DONE
+    // Mangler et tjek, men Getsymbol metoderne returner null values som kan bruges til at typecheck assignment
+    
     // 7
-    // Typecast precedence
-    // We need to implement an understanding of the types precedence. int --> float,  float --> string eg. basicly the implicit typecasting
-    // This is probably a feauture we will have to work on more, when we want to implement precedence for custom unit types.
-
-    // 8
     // Implement two global scope passes. One to catch free floating variables and next to store gloabl functions which type return need to be evauluated.
     // This requires local variables to be stored temporarly while evaluating the global.
     // Global varaibles --> Function types --> ok/notok
+    // To do
     
-    // Collecting functions and their return values
+    // 8
+    // Typecast precedence
+    // We need to implement an understanding of the types precedence. int --> float,  float --> string eg. basicly the implicit typecasting
+    // This is probably a feauture we will have to work on more, when we want to implement precedence for custom unit types.
+    // To do
+
     
     public override void InANewFunc(ANewFunc node) {
         
