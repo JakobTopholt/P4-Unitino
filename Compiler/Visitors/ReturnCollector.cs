54f8c4065d0b7ed93b28097b2ea37397e0071af6
Nima Moayedzadeh <57714574+Nima-Moayedzadeh@users.noreply.github.com>
2023-05-01 15:07:15 +0200

    Made cases for outAExpression and OutStatement nodes including control structures also refactored code

--- Compiler/Visitors/ReturnCollector.cs
+++ Compiler/Visitors/ReturnCollector.cs
@@ -5,7 +5,7 @@ namespace Compiler.Visitors;
 
 // Ikke sikkert der er brug for denne visitor T_T (idk yet)
 
-public class GlobalReturnCollector : DepthFirstAdapter
+public class ReturnCollector : DepthFirstAdapter
 {
     // Deal with untyped functions (figure out what symbol/type they return)
     // Deal with typed functions (Make sure that they actually return a value/symbol that corresponds to its defined type)
