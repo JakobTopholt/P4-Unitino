dd5133d6443d3df6b6c20237fe3bde18f116f890
Aavild <anders@aavild.dk>
2023-05-09 14:23:39 +0200

    Made tests more awesome

--- UnitTest/Data.cs
+++ UnitTest/Data.cs
@@ -11,14 +11,32 @@ public class Data
     public readonly string CodeGen;
     public readonly string Name;
     public readonly string Error;
-    public readonly bool ShouldFail;
-    public Data(Start? ast, string prettyPrint, string codeGen, string name, string error, bool shouldFail)
+    public readonly bool? ShouldFail;
+    public Data(string name, Start? ast, string error, string prettyPrint, string codeGen, bool? shouldFail)
     {
         Ast = ast;
+        Error = error;
         PrettyPrint = WhiteSpace.Replace(prettyPrint, "");
         CodeGen = codeGen.TrimStart().ReplaceLineEndings();
         Name = name;
-        Error = error;
         ShouldFail = shouldFail;
+    }
+    public Data(string name, Start? ast, string error, string prettyPrint, string codeGen)
+    {
+        Ast = ast;
+        Error = error;
+        PrettyPrint = WhiteSpace.Replace(prettyPrint, "");
+        CodeGen = codeGen.TrimStart().ReplaceLineEndings();
+        Name = name;
+        ShouldFail = null;
+    }
+    public Data(string name, Start? ast, string error, string prettyPrint)
+    {
+        Ast = ast;
+        Error = error;
+        PrettyPrint = WhiteSpace.Replace(prettyPrint, "");
+        CodeGen = "";
+        Name = name;
+        ShouldFail = null;
     }
 }
--- UnitTest/FilesToTestsConverter.cs
+++ UnitTest/FilesToTestsConverter.cs
@@ -12,23 +12,38 @@ public static class FilesToTestsConverter
 
     public static IEnumerable<TestCaseData> CodeGenData()
     {
-        return FileToTestCases().Select<Data, TestCaseData>(data => data.Ast != null
-            ? new TestCaseData(data.Ast, data.CodeGen).SetName(data.Name)
-            : new TestCaseData(data.Ast, data.CodeGen).SetName(data.Name).Ignore(data.Error));
+        return FileToTestCases().Aggregate(new List<TestCaseData>(), (list, data) =>
+        {
+            if (data.Ast == null)
+                list.Add(new TestCaseData(data.Ast, data.CodeGen).SetName(data.Name).Ignore(data.Error));
+            else if (!string.IsNullOrEmpty(data.CodeGen))
+                list.Add(new TestCaseData(data.Ast, data.CodeGen).SetName(data.Name));
+            return list;
+        });
     }
 
     public static IEnumerable<TestCaseData> PrettyPrintData()
     {
-        return FileToTestCases().Select<Data, TestCaseData>(data => data.Ast != null
-            ? new TestCaseData(data.Ast, data.PrettyPrint).SetName(data.Name)
-            : new TestCaseData(data.Ast, data.PrettyPrint).SetName(data.Name).Ignore(data.Error));
+        return FileToTestCases().Aggregate(new List<TestCaseData>(), (list, data) =>
+        {
+            if (data.Ast == null)
+                list.Add(new TestCaseData(data.Ast, data.PrettyPrint).SetName(data.Name).Ignore(data.Error));
+            else if (!string.IsNullOrEmpty(data.PrettyPrint))
+                list.Add(new TestCaseData(data.Ast, data.PrettyPrint).SetName(data.Name));
+            return list;
+        });
     }
 
     public static IEnumerable<TestCaseData> TypeVisitorData()
     {
-        return FileToTestCases().Select<Data, TestCaseData>(data => data.Ast != null
-            ? new TestCaseData(data.Ast, data.ShouldFail).SetName(data.Name)
-            : new TestCaseData(data.Ast, data.ShouldFail).SetName(data.Name).Ignore(data.Error));
+        return FileToTestCases().Aggregate(new List<TestCaseData>(), (list, data) =>
+        {
+            if (data.Ast == null)
+                list.Add(new TestCaseData(data.Ast, data.ShouldFail).SetName(data.Name).Ignore(data.Error));
+            else if (data.ShouldFail != null)
+                list.Add(new TestCaseData(data.Ast, data.ShouldFail).SetName(data.Name));
+            return list;
+        });
     }
 
     private static List<Data> FileToTestCases()
@@ -48,27 +63,75 @@ public static class FilesToTestsConverter
                 {
                     if (string.IsNullOrWhiteSpace(oneTestCaseString)) continue;
                     string[] test = oneTestCaseString.Split("////").Select(str => str.Trim()).ToArray();
-                    string name = test[0];
-                    string program = test[1];
-                    string prettyPrint = test[2];
-                    string codeGen = test[3];
-                    bool typedCorrect = test[4].Equals("true", StringComparison.OrdinalIgnoreCase);
-                    using MemoryStream stream = new(program.Select(c => (byte)c).ToArray());
-                    using StreamReader reader = new(stream);
-                    Start? ast = null;
-                    string error = "";
-                    try
+                    string name = fileName + ": " + test[0];
+                    switch (test.Length)
                     {
-                        ast = new Parser(new Lexer(reader)).Parse();
+                        case 0:
+                        case 1:
+                        case 2:
+                            continue;
+                        case 3:
+                        {
+                            string program = test[1];
+                            Start? ast = Start(program, out string error);
+                            
+                            string prettyPrint = test[2];
+                            TestData.Add(new Data(name, ast, error, prettyPrint));
+                            break;
+                        }
+                        case 4:
+                        {
+                            string program = test[1];
+                            Start? ast = Start(program, out string error);
+                            
+                            string prettyPrint = test[2];
+                            string codeGen = test[3];
+                            TestData.Add(new Data(name, ast, error, prettyPrint, codeGen));
+                            break;
+                        }
+                        default:
+                        {
+                            string program = test[1];
+                            Start? ast = Start(program, out string error);
+                            string prettyPrint = test[2];
+                            string codeGen = test[3];
+                            bool? typedCorrect = StringToBool(test[4]);
+                            TestData.Add(new Data(name, ast, error, prettyPrint, codeGen, typedCorrect));
+                            break;
+                        }
                     }
-                    catch (Exception e)
-                    {
-                        error = e.Message;
-                    }
-                    TestData.Add(new Data(ast, prettyPrint, codeGen,fileName + ": " + name, error, typedCorrect));
                 } 
             }
             return TestData;
         }
+    }
+
+    private static bool? StringToBool(string test)
+    {
+        return test.ToLower() switch
+        {
+            "true" => true,
+            "false" => false,
+            _ => null
+        };
+    }
+
+    private static Start? Start(string program, out string error)
+    {
+        Start ast;
+        using MemoryStream stream = new(program.Select(c => (byte)c).ToArray());
+        using StreamReader reader = new(stream);
+        try
+        {
+            ast = new Parser(new Lexer(reader)).Parse();
+        }
+        catch (Exception e)
+        {
+            error = e.Message;
+            return null;
+        }
+
+        error = "";
+        return ast;
     }
 }
