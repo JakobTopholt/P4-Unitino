aecb269841b27483ad0474e0fabac229f1f6c199
Aavild <anders@aavild.dk>
2023-05-10 16:30:45 +0200

    (untested) Added Arduino compiler, so now plugging in an Arduino board and running "ModuinoCompiler.exe inputFile" should compile the Moduino file to the board directly

---
+++ Compiler/ArduinoCompiler.cs
@@ -0,0 +1,88 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Compiler;
+
+internal static class ArduinoCompiler
+{
+    public static async Task DownloadCliAsync(string arduinoCliPath)
+    {
+        if (File.Exists(arduinoCliPath))
+            return;
+        Console.WriteLine("Downloading Arduino CLI");
+        
+        using HttpClient httpClient = new();
+        HttpResponseMessage response = await httpClient.GetAsync("https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh");
+        string scriptContent = await response.Content.ReadAsStringAsync();
+
+        Process process = new()
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = GetCommandInterpreter(),
+                Arguments = "-c \"" + scriptContent + "\"",
+                EnvironmentVariables = {["BINDIR"] = Directory.GetCurrentDirectory()}
+            }
+        };
+        process.Start();
+        await process.WaitForExitAsync();
+
+        Console.WriteLine("finished downloading with code " + process.ExitCode);
+    }
+
+    public static async Task InoToAVROnBoard(string filePath)
+    {
+        filePath += ".ino";
+        string boardsTable = await GetBoards();
+
+        // Parse the output to get the Fully Qualified Board Name(FQBN) and port name of the connected device
+        string[] rows = boardsTable.Split( Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+        foreach (string row in rows)
+        {
+            string[] column = row.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if (column.Length != 9 || column[2] != "Serial") 
+                continue;
+            string portName = column[0];
+            string boardFQBN = column[7];
+
+            Process compileProcess = await Compile(filePath, portName, boardFQBN);
+
+            Console.WriteLine(compileProcess.ExitCode == 0 ? "Compilation succeeded!" : "Compilation failed.");
+
+            break;
+        }
+    }
+    private static string GetCommandInterpreter()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            return "cmd.exe";
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            return "/bin/bash";
+        throw new Exception("Unsupported operating system.");
+    }
+
+    private static async Task<Process> Compile(string filePath, string portName, string boardFQBN)
+    {
+        Process compileProcess = new();
+        compileProcess.StartInfo.FileName = "arduino-cli";
+        compileProcess.StartInfo.Arguments = $"compile --fqbn {boardFQBN} --port {portName} {filePath}";
+        compileProcess.StartInfo.WorkingDirectory = Path.GetDirectoryName(filePath);
+        compileProcess.Start();
+        await compileProcess.WaitForExitAsync();
+        return compileProcess;
+    }
+
+    private static async Task<string> GetBoards()
+    {
+        Process listProcess = new();
+        listProcess.StartInfo.FileName = "arduino-cli";
+        listProcess.StartInfo.Arguments = "board list";
+        listProcess.StartInfo.UseShellExecute = false;
+        listProcess.StartInfo.RedirectStandardOutput = true;
+        listProcess.Start();
+
+        string output = await listProcess.StandardOutput.ReadToEndAsync();
+        await listProcess.WaitForExitAsync();
+        return output;
+    }
+}
---
+++ Compiler/ModuinoCompiler.cs
@@ -0,0 +1,39 @@
+﻿using Compiler.Visitors;
+using Moduino.lexer;
+using Moduino.node;
+using Moduino.parser;
+
+namespace Compiler;
+
+internal static class ModuinoCompiler
+{
+    public static async Task CompileMinoToIno(string inputFile)
+    {
+        Start start;
+        await using (FileStream fileStream = File.Open(inputFile + ".mino", FileMode.Open))
+        using (TextReader textReader = new StreamReader(fileStream))
+        {
+            Lexer lexer = new(textReader);
+            Parser parser = new(lexer);
+            start = parser.Parse();
+        }
+
+        start.Apply(new PrettyPrint());
+
+        List<SymbolTable> AllTables = new();
+
+        SymbolTable symbolTable = new(null, AllTables);
+        start.Apply(new UnitVisitor(symbolTable));
+        start.Apply(new FunctionVisitor(symbolTable));
+        start.Apply(new TypeChecker(symbolTable));
+
+        // Codegen Visitor
+        await using (FileStream stream = File.Create(Directory.GetCurrentDirectory() + "/../../../" + inputFile + ".ino"))
+        await using (StreamWriter writer = new(stream))
+        {
+            using CodeGen codegen = new(writer, symbolTable);
+            start.Apply(codegen);
+            await writer.FlushAsync();
+        }
+    }
+}
