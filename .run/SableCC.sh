cf415416057feabc3e02ea8b7a5bdbc0b1be8459
Aavild <anders@aavild.dk>
2023-05-27 12:58:32 +0200

    optimised the import of sablecc

--- .run/SableCC.sh
+++ .run/SableCC.sh
@@ -2,6 +2,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 mkdir "${SCRIPT_DIR}/../Compiler/SableCC"
-java -jar "${SCRIPT_DIR}/../sablecc-3-beta.3.altgen.20041114/lib/sablecc.jar" -t csharp ${SCRIPT_DIR}/../Compiler/grammar.sablecc3 -d Compiler/SableCC
+java -jar "${SCRIPT_DIR}/../sablecc/sablecc.jar" -t csharp ${SCRIPT_DIR}/../Compiler/grammar.sablecc3 -d Compiler/SableCC
 
 exec $SHELL # Remove # for verbose
--- sablecc-3-beta.3.altgen.20041114/src/org/sablecc/sablecc/Version.java
+++
@@ -1,6 +0,0 @@
-package org.sablecc.sablecc;
-
-public class Version
-{
-  public static final String VERSION = "3-beta.3.altgen.20041114";
-}
