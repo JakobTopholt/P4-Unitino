20acc32890961a28e1ff5775b3a494fffa16a202
Aavild <anders@aavild.dk>
2023-06-27 10:20:02 +0200

    Breakthrough in debugging

--- Compiler/Visitors/SymbolTable.cs
+++ Compiler/Visitors/SymbolTable.cs
@@ -39,6 +39,7 @@ public class SymbolTable
     public SymbolTable EnterScope()
     {
         currentScope++;
+        Console.Write(" " + (_currentTable + 1));
         if (++_currentTable < _allTables.Count)
         {
             _allTables[_currentTable]._currentTable = _currentTable;
@@ -53,6 +54,7 @@ public class SymbolTable
         currentScope--;
         if (_parent == null) 
             return this;
+        Console.Write(" /"+_currentTable + "(->" + _allTables.IndexOf(_parent) + ")");
         _parent._currentTable = _currentTable;
         _parent.currentScope = currentScope;
         return _parent;
@@ -60,6 +62,7 @@ public class SymbolTable
     }
     public SymbolTable ResetScope()
     {
+        Console.WriteLine();
         SymbolTable table = this;
         while (table._parent != null)
         {
